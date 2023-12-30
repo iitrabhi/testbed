1a6c6034490e6e01abed624af4170ab238b6d248
Abhinav Gupta <iitrabhi@gmail.com>
2023-12-30 17:41:52 -0600

    add a working search box

---
+++ src/1_webdev/1_flexbox_menu/js/suggestions.js
@@ -0,0 +1,23 @@
+// https://www.codingnepalweb.com/search-bar-autocomplete-search-suggestions-javascript/
+let suggestions = [
+    "What is the most challenging project you have worked on, and how did you tackle its challenges?",
+    "How do you ensure your code is clean and maintainable?",
+    "Explain how you would optimize a website's performance.",
+    "Discuss your experience with front-end technologies like HTML, CSS, and JavaScript.",
+    "How do you approach error handling and debugging in full stack development?",
+    "Describe your experience with back-end technologies and frameworks.",
+    "What are RESTful APIs, and how do you implement them in your projects?",
+    "Can you explain the concept of MVC architecture?",
+    "How do you manage database schema changes in a production environment?",
+    "What is your approach to security when developing a web application?",
+    "Explain how you would implement user authentication and authorization.",
+    "How do you keep up with the latest trends and technologies in full stack development?",
+    "Describe a time when you had to refactor code. What approach did you take?",
+    "What are containerization and virtualization, and how do they benefit development?",
+    "How do you ensure cross-browser and cross-platform compatibility?",
+    "Discuss how you use version control systems in your development workflow.",
+    "Explain the importance of responsive design and how you implement it.",
+    "How do you handle data validation and sanitation in web applications?",
+    "What are the advantages of using a CSS preprocessor?",
+    "Can you discuss a time when you had to optimize a slow-running query in a database?"
+];
